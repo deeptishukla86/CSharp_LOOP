623c81ad04a7e50b10eeb378a1a2214bcc2802d4
deeptishukla86 <deeptishukla086@gmail.com>
2021-08-23 15:38:35 +0530

    loop1

---
+++ while_LOOP/while_LOOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_LOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            System.Console.WriteLine("No from 1 to 10");
+            while(a<=10)
+            {
+                System.Console.WriteLine("print" + a);
+                a++;
+
+            }
+
+        }
+    }
+}
